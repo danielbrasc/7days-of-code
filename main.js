dfd0b8313779ccb3980441128a20c31fb114cfa7
danielbras <carlosbrasnascimento@gmail.com>
2022-05-31 21:41:44 -0300

    feat: added functionality to find a movie by title

--- main.js
+++ main.js
@@ -1,27 +1,66 @@
-import { api_key, base_url } from './env.js'
+import { api_key } from './environment/key.js'
 
-const movieContainer = document.querySelector(".movies")
+const moviesContainer = document.querySelector(".movies")
+const input = document.querySelector('input')
+const searchButton = document.querySelector('.searchIcon')
 
-window.onload = function () {
-  getPopularMovies()
+searchButton.addEventListener('click', searchMovie)
+
+input.addEventListener('keyup', function (event) {
+  if (event.keyCode == 13) {
+    searchMovie()
+    return
+  }
+})
+
+async function searchMovie() {
+  const inputValue = input.value
+  if (!!inputValue) {
+    cleanAllMovies()
+    const movies = await searchMovieByName(inputValue)
+    movies.forEach(movie => renderMovie(movie))
+  } else {
+    cleanAllMovies()
+    const movies = await getPopularMovies()
+    movies.forEach(movie => renderMovie(movie))
+  }
+}
+
+function cleanAllMovies() {
+  moviesContainer.innerHTML = ''
+}
+
+async function searchMovieByName(title) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${title}&page=1`
+  let response = await fetch(url)
+  const { results } = await response.json()
+
+  return results
 }
 
 async function getPopularMovies() {
   let url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
-  let response = await fetch(url);
-  const movies = await response.json();
+  let response = await fetch(url)
+  const { results } = await response.json()
 
-  movies.results.forEach((movie) => {
-    renderMovie(movie)
-  })
+  return results
 }
 
-async function renderMovie(movie) {
-  const { title, poster_path: image, backdrop_path: backupImage, vote_average: rating, release_date: year, overview: description } = movie;
+window.onload = async function () {
+  const movies = await getPopularMovies()
+  movies.forEach(movie => renderMovie(movie))
+}
+
+function renderMovie(movie) {
+  const { title, poster_path, vote_average: rating, release_date, overview: description } = movie;
+  const isFavorited = false
+
+  const year = new Date(release_date).getFullYear()
+  const image = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : 'https://criticalhits.com.br/wp-content/plugins/accelerated-mobile-pages/images/SD-default-image.png'
 
   const movieElement = document.createElement('li')
   movieElement.classList.add('movie')
-  movieContainer.appendChild(movieElement)
+  moviesContainer.appendChild(movieElement)
 
   const movieInformations = document.createElement('div')
   movieInformations.classList.add('movie-informations')
@@ -29,7 +68,7 @@ async function renderMovie(movie) {
   const movieImageContainer = document.createElement('div')
   movieImageContainer.classList.add('movie-image')
   const movieImage = document.createElement('img')
-  movieImage.src = `${base_url}${image ? image : backupImage}`
+  movieImage.src = image
   movieImage.alt = `${title} Poster`
   movieImageContainer.appendChild(movieImage)
   movieInformations.appendChild(movieImageContainer)
@@ -37,7 +76,7 @@ async function renderMovie(movie) {
   const movieTextContainer = document.createElement('div')
   movieTextContainer.classList.add('movie-text')
   const movieTitle = document.createElement('h4')
-  movieTitle.textContent = `${movie.title} (${year.slice(0, 4)})`
+  movieTitle.textContent = `${movie.title} (${year})`
   movieTextContainer.appendChild(movieTitle)
   movieInformations.appendChild(movieTextContainer)
 
@@ -60,7 +99,7 @@ async function renderMovie(movie) {
   const favorite = document.createElement('div')
   favorite.classList.add('favorite')
   const favoriteImage = document.createElement('img')
-  favoriteImage.src = false ? 'assets/heart-fill.svg' : 'assets/heart.svg'
+  favoriteImage.src = isFavorited ? 'assets/heart-fill.svg' : 'assets/heart.svg'
   favoriteImage.alt = 'Ícone de um coração'
   favoriteImage.classList.add('favorite-image')
   const favoriteText = document.createElement('span')
